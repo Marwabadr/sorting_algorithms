d0c71b18e0ab7fe68fbb0716c4c8bfcadd69e2bc
Aminafaraoui337 <aminafarraoui@gmail.com>
2024-01-23 07:25:32 -0800

    sort

--- sort.h
+++ sort.h
@@ -27,7 +27,7 @@ void insertion_sort_list(listint_t **list);
 /*functions for sorting*/
 void swap(int *a, int *b)
 int lomuto_partition(int *array, int low, int high, size_t size)
-void quicksort(int *array, int low, int high, size_t size)
+void quick_sort_recursive(int *array, int low, int high, size_t size)
 void quick_sort(int *array, size_t size)
 
 #endif
