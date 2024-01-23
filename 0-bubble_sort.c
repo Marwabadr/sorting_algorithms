114babc680a83e9c46b26ca68176a66359840344
Marwabadr <elbadrmarwa2002@gmail.com>
2024-01-23 04:25:31 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
 
 /**
-*bubble_sort - Sorts an array of integers
-*@array: The array to be sorted
-*@size: The size of the array.
+*bubble_sort - triage a bulle
+*@array: la table a trier
+*@size: la taille de la table
 */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
-	int swapped;
+	int tmp;
+	int swp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
+		swp = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = 1;
+				array[j + 1] = tmp;
+				swp = 1;
 				print_array(array, size);
 			}
 		}
-		if (swapped == 0)
+		if (swp == 0)
 			break;
 	}
 }
