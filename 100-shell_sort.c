a7c08d9afccbba637e51a0132d57dbfc34c699e8
Aminafaraoui337 <aminafarraoui@gmail.com>
2024-01-23 09:34:29 -0800

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,34 +11,32 @@ void swap(int *a, int *b)
         *b = temp;
 }
 /**
- *shell_sort - Sorts an array of integers in ascending order using Shell sort
- *algorithm with Knuth sequence.
- *@array: The array to be sorted
- *@size: The size of the array
+ * shell_sort - Sorts an array of integers in ascending order using
+ *the Shell sort algorithm with the Knuth sequence.
+ *@array: The array to be sorted.
+ *@size: The size of the array.
  */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i, j;
-	int temp;
-
+       
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
+
 	while (gap > 0)
 	{
-		print_array(array, size);
-
 		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
+			int temp = array[i];
+			
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			{
-				array[j] = array[j - gap];
+				swap(&array[j], &array[j - gap]);
 			}
-			array[j] = temp;
 		}
 
+		print_array(array, size);
+		
 		gap = (gap - 1) / 3;
 	}
-
-	print_array(array, size);
 }
