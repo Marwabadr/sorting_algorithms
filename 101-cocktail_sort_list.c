40513e878c2aecc95a91b02cd8643a3a2b3a01d5
Aminafaraoui337 <aminafarraoui@gmail.com>
2024-01-23 11:15:20 -0800

    cocktail sort list

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,18 +7,18 @@
  */
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-        if (node1->prev)
-                node1->prev->next = node2;
-        else
-                *list = node2;
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		-list = node2;
 
-        if (node2->next)
-                node2->next->prev = node1;
+	if (node2->next)
+		ode2->next->prev = node1;
 
-        node1->next = node2->next;
-        node2->prev = node1->prev;
-        node1->prev = node2;
-        node2->next = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 }
 /**
  *cocktail_sort_list - Sorts a doubly linked list of integers
