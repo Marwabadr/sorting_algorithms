91e98fe9e64c149fc2f573ec67e2e2da350ce972
Aminafaraoui337 <aminafarraoui@gmail.com>
2024-01-23 13:23:36 -0800

    cocktail sort list

--- sort.h
+++ sort.h
@@ -33,5 +33,5 @@ void quick_sort(int *array, size_t size);
 /*function for shell sort*/
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t *noeud);
+listint_t swap_nodes(listint_t **list, listint_t *noeud);
 #endif
